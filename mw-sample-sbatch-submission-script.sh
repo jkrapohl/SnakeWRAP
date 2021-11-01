2cf5f35f09b0281bceb9c68b1d19721c49c67614
jkrapohl <93545086+jkrapohl@users.noreply.github.com>
2021-11-01 16:02:48 -0600

    Update mw-sample-sbatch-submission-script.sh

--- mw-sample-sbatch-submission-script.sh
+++ mw-sample-sbatch-submission-script.sh
@@ -12,7 +12,7 @@ export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 #initialize conda and activate environment
-module load miniconda3
+#module load miniconda3
 #source path/to/source/bin
 #source activate metawrap-env
 
